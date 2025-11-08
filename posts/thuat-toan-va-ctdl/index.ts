0343b57f9909f3cddc208837da78fe0f239f05b2
HanitaV <phonganotheracc@gmail.com>
2025-11-08 21:29:05 +0700

    Thuật toán do anh bvd soạn

--- posts/thuat-toan-va-ctdl/index.ts
+++ posts/thuat-toan-va-ctdl/index.ts
@@ -1,7 +1,4 @@
-// posts/thuat-toan-va-ctdl/index.ts
-//import { post as quyHoachDong } from './algo001';
-
-
-//export const thuatToanPosts = [
-//  quyHoachDong,
-//];
+import { post as thng_k_cc_thut_ton_t_lp_3_n_lp_12 } from './thng-k-cc-thut-ton-t-lp-3-n-lp-12';
+export const thuatToanVaCtdlPosts = [
+  thng_k_cc_thut_ton_t_lp_3_n_lp_12,
+];
---
+++ posts/thuat-toan-va-ctdl/thng-k-cc-thut-ton-t-lp-3-n-lp-12.ts
@@ -0,0 +1,59 @@
+// posts/thuat-toan-va-ctdl/thng-k-cc-thut-ton-t-lp-3-n-lp-12.ts
+export const post = `
+---
+id: "thu482"
+slug: "thng-k-cc-thut-ton-t-lp-3-n-lp-12"
+title: "Thống kê các thuật toán từ lớp 3 đến lớp 12"
+excerpt: ""
+author: "Bùi Việt Dũng"
+publishedDate: "2025-11-08"
+readTime: 10
+categoryId: "thuat-toan-va-ctdl"
+tags: '[thuật toán, lập trình]'
+level: "Cơ bản"
+grade: 12
+language: Vietnamese
+attachments: '[]'
+downloads: 0
+---
+## Thống kê các thuật toán từ lớp 3 đến lớp 12
+Lớp 3-5: 
+Cộng, trừ, nhân, chia (ngôn ngữ giới HSG gọi là Bignum)
+Tính trung bình cộng
+Sắp xếp đếm (counting sort)
+
+Lớp 6
+Kiểm tra số nguyên tố. Phân tích một số ra thừa số nguyên tố.
+Liệt kê / đếm số ước của một số.
+Tính ước chung lớn nhất, bội chung nhỏ nhất
+Lớp 7
+Tính số thập phân vô hạn tuần hoàn
+Thuật toán tìm kiếm tuần tự, thuật toán tìm kiếm nhị phân
+Thuật toán sắp xếp chọn
+Lớp 8
+Thuật toán sắp xếp nổi bọt
+Thuật toán giải phương trình bậc nhất một ẩn, phương trình bậc hai một ẩn, hệ phương trình bậc nhất hai ẩn
+Thuật toán Euclide mở rộng tìm nghiệm nguyên của phương trình bậc nhất hai ẩn.
+
+Lớp 10
+Phi hàm Euler
+Đổi hệ cơ số
+Tính hàm băm của một xâu
+Các bài toán liên quan đến hình học giải tích.
+
+Lớp 11
+Tìm kiếm tuần tự, tìm kiếm nhị phân
+Sắp xếp chèn, sắp xếp nổi bọt, sắp xếp nhanh, sắp xếp trộn
+Tính số fibonacci bằng đệ quy
+Tháp Hà Nội
+Tính lũy thừa a^n trong O(log n)
+Thuật toán Karatsuba để nhân đa thức trong O(n^(log_3 (2))
+Cây nhị phân tìm kiếm.
+Quay lui để sinh hoán vị, chỉnh hợp, tổ hợp.
+Danh sách liên kết
+
+Lớp 12
+Ngăn xếp, hàng đợi
+DFS/ BFS
+Tìm thành phần liên thông mạnh của đồ thị.
+`;

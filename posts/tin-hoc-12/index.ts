66d23d9241078669db248d361ce16067f02bab1d
Nguyễn Duy Khánh <89499196+Desithedev@users.noreply.github.com>
2025-11-08 13:01:34 +0700

    xóa bài cũ

--- posts/tin-hoc-12/index.ts
+++ posts/tin-hoc-12/index.ts
@@ -1,12 +1,7 @@
 // posts/tin-hoc-12/index.ts
-import { post as vectorStlCpp } from './cpp002';
-import { post as oopPython } from './py003';
-import { post as mangMotChieu } from './tin12-001';
+
 import { post as huongNghiep12G } from './12g';
 
 export const tinHoc12Posts = [
-  vectorStlCpp,
-  oopPython,
-  mangMotChieu,
   huongNghiep12G,
 ];
